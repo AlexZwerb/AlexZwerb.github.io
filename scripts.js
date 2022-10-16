1e4520581f0c64de4a877704aef1b3a26b529779
AlexZwerb <vasilijsidorov62@gmail.com>
2022-10-16 19:14:57 +0300

    AlexZfoto3

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
-var bczh = document.getElementById("bczh");
 function wiki(x){
 var ssilka=open(x,'_blank',"top=0 width=400 height=200");
 	
+}
+var answ=document.getElementById("answer");	
+function TITLE(y){
+ alert(y);	
 }
