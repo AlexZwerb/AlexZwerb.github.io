903cf8cae2c8631ffdcfc4c6d9f85c832738a57d
AlexZwerb <vasilijsidorov62@gmail.com>
2022-10-16 18:48:50 +0300

    AlexZplus2

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 var bczh = document.getElementById("bczh");
 function wiki(x){
-var ssilka=open(x,'_blank',"top=0 width=900 height=500");
+var ssilka=open(x,'_blank',"top=0 width=400 height=200");
 	
 }
